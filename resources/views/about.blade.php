132e8d891a0457e28ded5e8c2622609adcb5a999
Developer-Abel <55603971+Developer-Abel@users.noreply.github.com>
2021-01-30 06:00:56 -0600

    nombrando rutas

---
+++ resources/views/about.blade.php
@@ -0,0 +1,1 @@
+<h1>a cerca de </h1>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<html>
+   <head>
+      <title>Home</title>
+   </head>
+   <body>
+      <nav>
+         <ul>
+            <li><a href="{{route('/')}}">Home</a></li>
+            <li><a href="{{route('acerca')}}">About</a></li>
+            <li><a href="{{route('portafolio')}}">Portafolio</a></li>
+            <li><a href="{{route('contacto')}}">Contacto</a></li>
+         </ul>
+      </nav>
+      <h1>home</h1>
+      Bienvenido
+   </body>
+</html>
